417a5ae6a15fd9cf186899004e7c3da1d03630d1
Aoyagibeautifulsea <summerhorse1123@outlook.jp>
2023-06-07 17:59:07 +0900

    syosi

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -11,5 +12,10 @@ class PostController extends Controller
 {
     return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
 }
-
+public function show(Post $post)
+{
+    dd($post);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
 }
+}
+?>
